cc58fbae11f24ec40bfb6f5f2c6896e5d305b536
SunKyu <leesunkeuy@naver.com>
2016-05-10 16:11:39 +0900

    read servicekey from file
    read servicekey from file

--- src/app.js
+++ src/app.js
@@ -1,29 +1,35 @@
 var request = require('request');
 var express = require('express');
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 mongoose.connect('mongodb://localhost/atmos')
 
 var app = express()
-
-
-
+var servicekey = fs.readFileSync("./servicekey.txt", "utf8")
+servicekey = servicekey.replace(/(\r\n|\n|\r)/gm,"");
 
 var url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty';
-var queryParams = '?' + encodeURIComponent('ServiceKey') + '=My4MBr5BwgTqOpiGFM1X0GQNMYc%2BCB2ZXHUIKsnCmrPstaY%2BIR5e4fsBZIrrJ8WZvS9GSXmnxMa%2BO7mefmEEEg%3D%3D'; /* Service Key*/
+var queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + String(servicekey); /* Service Key*/
 queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /* 한 페이지 결과 수 */
 queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* 페이지 번호 */
 queryParams += '&' + encodeURIComponent('sidoName') + '=' + encodeURIComponent('서울'); /* sidoName */
 queryParams += '&' + encodeURIComponent('_returnType') + '=' + encodeURIComponent('json'); //return type
+var data = "";
+request({url: url + queryParams, method: 'GET'}, function (error, response, body){
+                    data = body;
+});
 
 app.get('/', function(req, res){
     console.log ("runing")
-    request({
+  /*  request({
         url: url + queryParams,
         method: 'GET'
     }, function (error, response, body) {
         res.send(body)
     });
+    */
+   res.send(data);
 });
 
 app.listen(3000);
